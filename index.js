7086b09be370f0e6e67b0bd4c24601727c652ced
johnfliu <johnfliu@coolimage.com>
2016-12-12 22:47:53 -0500

    Standard encryption 2012 working now

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var stdenc2009 = require('./standard_encryption_2009');
+var stdenc2012 = require('./standard_encryption_2012');
 
 
 function encrypt2009(site, city, pass) {
@@ -15,7 +16,13 @@ function encrypt2009b(site, city, pass) {
   return str;
 }
 
+function encrypt2012(site, city, pass) {
+  var unihash = stdenc2012.unihash(city, pass); //city,pass (city in lowercase)
+  var str = stdenc2012.calc2012(unihash, site.toLowerCase());
+  return str;
+}
+
 // sample usage via console
 if (process.argv.length > 4) {
-  console.log(encrypt2009b(process.argv[2], process.argv[3], process.argv[4]));
+  console.log(encrypt2012(process.argv[2], process.argv[3], process.argv[4]));
 }
---
+++ standard_encryption_2012.js
@@ -0,0 +1,25 @@
+var sha256 = require('./coolimage_sha256');
+var encoder = require('./coolimage_encoder');
+
+exports.unihash = unihash;
+exports.calc2012 = calc2012;
+
+function hash(str) {
+	var binarray = sha256.sha(str + '@coolimage.com', true);
+	var binstr = encoder.binarray2str(binarray);
+	var b64 = encoder.base64_encode(binstr);
+	return b64;
+}
+
+// convert city,pass into unihash
+// city in lowercase
+function unihash(city, pass) {
+	return hash(city + ',' + pass);
+}
+
+// use unihash and site_domain to generate site pass
+function calc2012(unihash, site_domain) {
+	var raw_sitepass = hash(unihash + site_domain);
+	// replace the 3 special chars with 0, because numbers are harder to come by
+	return raw_sitepass.replace('+', '0').replace('/', '0').replace('=', '0').substr(0, 16);
+}
